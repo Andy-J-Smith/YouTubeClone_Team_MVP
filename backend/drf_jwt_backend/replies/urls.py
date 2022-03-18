f9d75cf48f92a22c5871a8a259fcdcc828ee283e
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-03-18 12:01:57 -0500

    updated replies views+Update postman JSON file

--- backend/drf_jwt_backend/replies/urls.py
+++ backend/drf_jwt_backend/replies/urls.py
@@ -4,6 +4,6 @@ from replies import views
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
 urlpatterns = [
-    path('<int:comment>', views.user_reply),
+    path('<int:comment>/', views.user_reply),
     path('all/', views.get_all_replies),
 ]
