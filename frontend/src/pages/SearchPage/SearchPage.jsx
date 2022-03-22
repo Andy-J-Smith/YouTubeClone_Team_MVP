1fbdae72f387db786f4d1b14ae2d78a023edf1fb
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-03-22 10:50:03 -0500

    displayed video id on search page

--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -6,6 +6,7 @@ import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 const SearchPage = (props) => {
   const [searchResults, setSearchResults] = useState([]);
   const [videoId, setVideoId] = useState("dQw4w9WgXcQ");
+
   const asApi = 'AIzaSyB--WnZi-41d2SSGsccN9FHWgPsp_Erh4I';
   const afApi = 'AlzaSyBKkwwk2xiNqsE_mrtJ3q6zByuYJTbTJms';
   const jpApi = 'AlzaSyBeDuwLRCEuSKbuXL_x1QED9VXNdipYR_M';
@@ -14,22 +15,38 @@ const SearchPage = (props) => {
     let response = await axios.get(
       `https://www.googleapis.com/youtube/v3/search?q=${search}&key=${asApi}&part=snippet&maxResults=4&type=video`
     );
-    console.log("Search results ", response.data);
-    setSearchResults(response.data);
+    console.log("Search results ", response.data.items);
+    setSearchResults(response.data.items);
   }
 
-  // function getVideoID(){
-  //   return searchResults.map(searchResults =>
-  //     <VideoPlayer
-  //     key={videoId.id.videoId})
+  async function getRelatedVideos(videoId){
+    let response = await axios.get (`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${searchResults.id.videoId}&type=video&key=${asApi}&maxResults=4&part=snippet`)
+    console.log("video id ", response.data.items);
+    setVideoId(response.data.items);
+  }
 
-  // }
+  // useEffect(() => {
+  //   getSearchResults();
+  // }, []);
+
 
   return (
     <div>
       <SearchBar getSearchResults={getSearchResults} />
       <VideoPlayer videoId={videoId} />
-      <p>{searchResults.data ? <h2>{searchResults.data.title}</h2> : null }</p>
+      <table>
+        <tbody>
+          {searchResults.map((searchResults, index)=> {
+            return (
+              <tr className="row" key={index}>
+                <td>{searchResults.id.videoId}</td>
+              </tr>
+            )
+          })}
+
+        </tbody>
+      </table>
+    
     </div>
   );
 };
